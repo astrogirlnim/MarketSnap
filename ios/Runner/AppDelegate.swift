84dd3c6ba4d6804c0dcbe8d2381716407e8bb2e0
N <nmmsoftware@gmail.com>
2025-06-24 06:36:14 -0500

    fix(background): improve callbackDispatcher logging and task handling

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import workmanager
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,15 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Register the background task
+    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "syncPendingMediaTask")
+    
+    // Register for other plugins
     GeneratedPluginRegistrant.register(with: self)
+
+    // Register a periodic task for iOS
+    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "1", frequency: NSNumber(value: 900)) // 15 minutes
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
