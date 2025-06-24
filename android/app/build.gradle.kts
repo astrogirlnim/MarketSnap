f163e4bd08a45f2f72d774be0f34a7dff7c7a294
N <nmmsoftware@gmail.com>
2025-06-24 00:42:39 -0500

    Fix Android build error by updating SDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.marketsnap"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -27,9 +27,9 @@ android {
         applicationId = "com.example.marketsnap"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
         versionName = flutter.versionName
     }
 
